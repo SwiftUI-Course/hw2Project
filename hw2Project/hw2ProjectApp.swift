0b4de3748dbf93036fc5c643754685baaf884bf2
jackliu999 <jackliu3851@gmail.com>
2023-06-22 17:17:36 -0400

    Initial Commit

---
+++ hw2Project/hw2ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw2ProjectApp.swift
+//  hw2Project
+//
+//  Created by Jack Liu on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct hw2ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
